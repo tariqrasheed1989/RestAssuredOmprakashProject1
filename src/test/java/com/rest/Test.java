f69ee8a995f0ce33dd00139b3c1377ca4a9de2d3
tariq <tariq@gmail.com>
2024-03-05 20:30:44 +0530

    Day 2

--- src/test/java/com/rest/Test.java
+++ src/test/java/com/rest/Test.java
@@ -1,12 +1,16 @@
 package com.rest;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class Test {
     @org.testng.annotations.Test
     public void test(){
-        given().
+        String name=given().
                 baseUri("https://api.postman.com").
                 header("x-api-key","PMAK-65e339c5019ed6000182dc92-48669707f840a270dd3ae55f71ad77b0c9").
         when().
@@ -15,11 +19,14 @@ public class Test {
                 log().all().
                 assertThat().
                 statusCode(200).
-                body("workspaces.name",hasItems("My Workspace","SDET-QA Postman Practice","UdemyPractice","OmprakashChavan","TOBEDELETED"),
-                        "workspaces.type",hasItems("personal", "personal", "team", "team", "team"),
-                        "workspaces[0].name",is(equalTo("My Workspace")),
-                           "workspaces.size()",equalTo(5),
-                            "workspaces.name",hasItems("My Workspace"));
+                extract().
+                response().path("workspaces[0].name");
+                System.out.println("workspace name = "+ name);
+
+                assertThat(name,equalTo("My Workspace"));
+//        JsonPath jsonPath=new JsonPath(res.asString());
+//        System.out.println("workspace name = "+jsonPath.getString("workspaces[0].name"));
+//        System.out.println("workspace name = "+res.path("workspaces[0].name"));
 
     }
 }
